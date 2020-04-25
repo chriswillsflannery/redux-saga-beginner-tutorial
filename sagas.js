ac38d07f1f9253317b929efb77f9f2619ff5912c
Chris Flannery <chriswillsflannery@gmail.com>
2020-04-25 18:16:17 -0400

    created first sagas

---
+++ sagas.js
@@ -0,0 +1,26 @@
+import { put, takeEvery, all } from 'redux-saga/effects'
+
+const delay = ms => new Promise(res => setTimeout(res, ms))
+
+export function* helloSaga() {
+  console.log('Hello Sagas!');
+}
+
+// worker saga
+export function* incrementAsync() {
+  yield delay(1000)
+  yield put({ type: 'INCREMENT' })
+}
+
+// watcher saga
+export function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+// entry point watcher saga
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
